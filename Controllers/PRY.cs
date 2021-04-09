dd24ed08aac65b2b06cbea7b59318ac7fb1a82b3
Adriancito303 <70454480+Adriancito303@users.noreply.github.com>
2021-04-08 18:36:07 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/PRY.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_CésarSilva1184519_JonnathanLanuza1082219.Controllers
+{
+    public class PRY : Controller
+    {
+        // GET: PRY
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PRY/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PRY/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PRY/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PRY/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PRY/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PRY/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PRY/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
